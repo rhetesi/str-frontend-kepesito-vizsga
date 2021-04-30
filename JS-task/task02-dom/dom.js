6440705acde5910370c2af53c94e3bba7ea31b44
rhetesi <rhetesi@gmail.com>
2021-04-30 11:08:36 +0200

    JS-task02-DOM works - DONE

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,23 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    const navLink = document.querySelectorAll('nav#link-list a');
+    navLink.forEach(item => {
+        if (item.innerHTML.includes('outer-link')) {
+            item.setAttribute('target', '_blank');
+            item.innerHTML = `<strong>${item.innerHTML}</strong>`;
+        }
+    })
 
+    document.querySelectorAll('nav').forEach((elem) => {
+        elem.style.display = 'flex';
+        elem.style.flexDirection = 'column';
+        elem.style.margin = '0px auto';
+        elem.style.width = '30%';
+        elem.style.border = '1px solid blue';
+        elem.style.padding = '16px';
+    })
+}
+changeOuterLinks()
 
-export { changeOuterLinks };
+export {
+    changeOuterLinks
+};
